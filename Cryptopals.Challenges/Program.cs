e9cadf6936ae6593e9fa08e36e4c17e9ea787232
Daniel Hughes <ilikepie355113@gmail.com>
2019-01-23 13:26:53 -0700

    Add project files.

---
+++ Cryptopals.Challenges/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptopals.Challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
