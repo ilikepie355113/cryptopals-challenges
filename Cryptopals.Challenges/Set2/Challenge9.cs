4d66299b73363f6938cff8a874c7313b2296b897
Daniel Hughes <ilikepie355113@gmail.com>
2019-02-01 13:41:55 -0700

    Add challenge9

---
+++ Cryptopals.Challenges/Set2/Challenge9.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cryptopals.Utils;
+
+namespace Cryptopals.Challenges.Set2
+{
+    public class Challenge9
+    {
+        private readonly string _input;
+        private readonly int _size;
+
+        public Challenge9(string input, int size)
+        {
+            _input = input;
+            _size = size;
+        }
+
+        public string SolveChallenge()
+        {
+            return PadBlock(_input.ToByteArray(), _size).ToASCIIString();
+        }
+
+        private byte[] PadBlock(byte[] block, int size)
+        {
+            byte[] paddedBlock = new byte[size];
+            int paddingLength = size - block.Length;
+            block.CopyTo(paddedBlock, 0);
+
+            for (int i = 1; i <= paddingLength; i++)
+            {
+                paddedBlock[size - i] = (byte) paddingLength;
+            }
+
+            return paddedBlock;
+        }
+    }
+}
---
+++ Cryptopals.Tests/Set2/Challenge9Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Cryptopals.Utils;
+
+namespace Cryptopals.Tests.Set2
+{
+    public class Challenge9Tests
+    {
+        [Fact]
+        public void SolveChallenge_ShouldSolveChallengeInput()
+        {
+            string input = "YELLOW SUBMARINE";
+            int size = 20;
+            string expected = "YELLOW SUBMARINE\u0004\u0004\u0004\u0004";
+
+            string actual = new Challenges.Set2.Challenge9(input, size).SolveChallenge();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
